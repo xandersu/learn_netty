87ee1480b4dda1a66c7556650dcc5cb9da53c827
suxun <suxun@gotokeep.com>
2019-02-19 22:59:08 +0800

    Initial commit

---
+++ src/main/java/com/xandersu/learn_netty/LearnNettyApplication.java
@@ -0,0 +1,13 @@
+package com.xandersu.learn_netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnNettyApplication.class, args);
+    }
+
+}
